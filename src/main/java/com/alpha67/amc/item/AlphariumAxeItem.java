d89517b6878b0d4f10954ce136d630368d8e34cd
vultorio67 <68811816+vultorio67@users.noreply.github.com>
2021-12-23 10:17:54 +0100

    update

---
+++ src/main/java/com/alpha67/amc/item/AlphariumAxeItem.java
@@ -0,0 +1,52 @@
+
+package com.alpha67.amc.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.AxeItem;
+
+import com.alpha67.amc.itemgroup.AlphatabItemGroup;
+import com.alpha67.amc.AmcModElements;
+
+@AmcModElements.ModElement.Tag
+public class AlphariumAxeItem extends AmcModElements.ModElement {
+	@ObjectHolder("amc:alpharium_axe")
+	public static final Item block = null;
+	public AlphariumAxeItem(AmcModElements instance) {
+		super(instance, 6);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new AxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 3500;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 4;
+			}
+
+			public int getEnchantability() {
+				return 15;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(AlphariumingotItem.block));
+			}
+		}, 1, -3f, new Item.Properties().group(AlphatabItemGroup.tab)) {
+		}.setRegistryName("alpharium_axe"));
+	}
+}
---
+++ src/main/java/com/alpha67/amc/world/biome/AlphaBiomBiome.java
@@ -0,0 +1,88 @@
+
+package com.alpha67.amc.world.biome;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.BiomeManager;
+
+import net.minecraft.world.gen.trunkplacer.MegaJungleTrunkPlacer;
+import net.minecraft.world.gen.treedecorator.TrunkVineTreeDecorator;
+import net.minecraft.world.gen.treedecorator.LeaveVineTreeDecorator;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.NoiseDependant;
+import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
+import net.minecraft.world.gen.foliageplacer.JungleFoliagePlacer;
+import net.minecraft.world.gen.feature.TwoLayerFeature;
+import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.gen.feature.FeatureSpread;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.biome.BiomeGenerationSettings;
+import net.minecraft.world.biome.BiomeAmbience;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.block.Blocks;
+
+import com.google.common.collect.ImmutableList;
+
+import com.alpha67.amc.AmcModElements;
+
+@AmcModElements.ModElement.Tag
+public class AlphaBiomBiome extends AmcModElements.ModElement {
+	public static Biome biome;
+	public AlphaBiomBiome(AmcModElements instance) {
+		super(instance, 14);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new BiomeRegisterHandler());
+	}
+	private static class BiomeRegisterHandler {
+		@SubscribeEvent
+		public void registerBiomes(RegistryEvent.Register<Biome> event) {
+			if (biome == null) {
+				BiomeAmbience effects = new BiomeAmbience.Builder().setFogColor(12638463).setWaterColor(-16764109).setWaterFogColor(-16764109)
+						.withSkyColor(7972607).withFoliageColor(-13434829).withGrassColor(-16777216).build();
+				BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder()
+						.withSurfaceBuilder(SurfaceBuilder.DEFAULT.func_242929_a(new SurfaceBuilderConfig(Blocks.GRASS_BLOCK.getDefaultState(),
+								Blocks.DIRT.getDefaultState(), Blocks.DIRT.getDefaultState())));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.TREE
+						.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.JUNGLE_LOG.getDefaultState()),
+								new SimpleBlockStateProvider(Blocks.JUNGLE_LEAVES.getDefaultState()),
+								new JungleFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(0), 2),
+								new MegaJungleTrunkPlacer(10, 2, 19), new TwoLayerFeature(1, 1, 2)))
+										.setDecorators(
+												ImmutableList.of(TrunkVineTreeDecorator.field_236879_b_, LeaveVineTreeDecorator.field_236871_b_))
+										.build())
+						.withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT)
+						.withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(1, 0.1F, 1))));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.RANDOM_PATCH.withConfiguration(Features.Configs.GRASS_PATCH_CONFIG).withPlacement(Features.Placements.PATCH_PLACEMENT)
+								.withPlacement(Placement.COUNT_NOISE.configure(new NoiseDependant(-0.8D, 5, 2))));
+				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+						Feature.FLOWER.withConfiguration(Features.Configs.NORMAL_FLOWER_CONFIG)
+								.withPlacement(Features.Placements.VEGETATION_PLACEMENT).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT)
+								.func_242731_b(4));
+				DefaultBiomeFeatures.withCavesAndCanyons(biomeGenerationSettings);
+				DefaultBiomeFeatures.withFrozenTopLayer(biomeGenerationSettings);
+				MobSpawnInfo.Builder mobSpawnInfo = new MobSpawnInfo.Builder().isValidSpawnBiomeForPlayer();
+				biome = new Biome.Builder().precipitation(Biome.RainType.RAIN).category(Biome.Category.NONE).depth(0.5f).scale(0.7f).temperature(0.5f)
+						.downfall(0.7f).setEffects(effects).withMobSpawnSettings(mobSpawnInfo.copy())
+						.withGenerationSettings(biomeGenerationSettings.build()).build();
+				event.getRegistry().register(biome.setRegistryName("amc:alpha_biom"));
+			}
+		}
+	}
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+		BiomeManager.addBiome(BiomeManager.BiomeType.WARM,
+				new BiomeManager.BiomeEntry(RegistryKey.getOrCreateKey(Registry.BIOME_KEY, WorldGenRegistries.BIOME.getKey(biome)), 1024));
+	}
+}
