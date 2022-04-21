fa817d780fd236253f9275480d665d5e708d1468
Nitrosi <90981533+Nitrosi@users.noreply.github.com>
2022-04-21 20:34:13 +0000

    basic wire added

---
+++ src/main/java/com/alpha67/amc/block/BasicwireflateBlock.java
@@ -0,0 +1,430 @@
+
+package com.alpha67.amc.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.items.wrapper.SidedInvWrapper;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.energy.EnergyStorage;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Direction;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.LockableLootTileEntity;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.network.play.server.SUpdateTileEntityPacket;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.BlockItem;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.ChestContainer;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.DirectionalBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import javax.annotation.Nullable;
+
+import java.util.stream.Stream;
+import java.util.stream.IntStream;
+import java.util.Random;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.AbstractMap;
+
+import com.alpha67.amc.procedures.ProcessbasicwireProcedure;
+import com.alpha67.amc.AmcModElements;
+
+@AmcModElements.ModElement.Tag
+public class BasicwireflateBlock extends AmcModElements.ModElement {
+	@ObjectHolder("amc:basicwireflate")
+	public static final Block block = null;
+	@ObjectHolder("amc:basicwireflate")
+	public static final TileEntityType<CustomTileEntity> tileEntityType = null;
+
+	public BasicwireflateBlock(AmcModElements instance) {
+		super(instance, 162);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new TileEntityRegisterHandler());
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(null)).setRegistryName(block.getRegistryName()));
+	}
+
+	private static class TileEntityRegisterHandler {
+		@SubscribeEvent
+		public void registerTileEntity(RegistryEvent.Register<TileEntityType<?>> event) {
+			event.getRegistry().register(TileEntityType.Builder.create(CustomTileEntity::new, block).build(null).setRegistryName("basicwireflate"));
+		}
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+
+	public static class CustomBlock extends Block {
+		public static final DirectionProperty FACING = DirectionalBlock.FACING;
+
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).notSolid()
+					.setOpaque((bs, br, bp) -> false));
+			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+			setRegistryName("basicwireflate");
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 0;
+		}
+
+		@Override
+		public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
+			Vector3d offset = state.getOffset(world, pos);
+			switch ((Direction) state.get(FACING)) {
+				case SOUTH :
+				default :
+					return VoxelShapes.or(makeCuboidShape(10, 6, 16, 6, 10, 0), makeCuboidShape(16, 6, 10, 0, 10, 6)
+
+					)
+
+							.withOffset(offset.x, offset.y, offset.z);
+				case NORTH :
+					return VoxelShapes.or(makeCuboidShape(6, 6, 0, 10, 10, 16), makeCuboidShape(0, 6, 6, 16, 10, 10)
+
+					)
+
+							.withOffset(offset.x, offset.y, offset.z);
+				case EAST :
+					return VoxelShapes.or(makeCuboidShape(16, 6, 6, 0, 10, 10), makeCuboidShape(10, 6, 0, 6, 10, 16)
+
+					)
+
+							.withOffset(offset.x, offset.y, offset.z);
+				case WEST :
+					return VoxelShapes.or(makeCuboidShape(0, 6, 10, 16, 10, 6), makeCuboidShape(6, 6, 16, 10, 10, 0)
+
+					)
+
+							.withOffset(offset.x, offset.y, offset.z);
+				case UP :
+					return VoxelShapes.or(makeCuboidShape(6, 16, 6, 10, 0, 10), makeCuboidShape(0, 10, 6, 16, 6, 10)
+
+					)
+
+							.withOffset(offset.x, offset.y, offset.z);
+				case DOWN :
+					return VoxelShapes.or(makeCuboidShape(6, 0, 10, 10, 16, 6), makeCuboidShape(0, 6, 10, 16, 10, 6)
+
+					)
+
+							.withOffset(offset.x, offset.y, offset.z);
+			}
+		}
+
+		@Override
+		protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+			builder.add(FACING);
+		}
+
+		public BlockState rotate(BlockState state, Rotation rot) {
+			return state.with(FACING, rot.rotate(state.get(FACING)));
+		}
+
+		public BlockState mirror(BlockState state, Mirror mirrorIn) {
+			return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+		}
+
+		@Override
+		public BlockState getStateForPlacement(BlockItemUseContext context) {
+			;
+			return this.getDefaultState().with(FACING, context.getNearestLookingDirection().getOpposite());
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(BasicWireBlock.block));
+		}
+
+		@Override
+		public void onBlockAdded(BlockState blockstate, World world, BlockPos pos, BlockState oldState, boolean moving) {
+			super.onBlockAdded(blockstate, world, pos, oldState, moving);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			world.getPendingBlockTicks().scheduleTick(pos, this, 1);
+		}
+
+		@Override
+		public void tick(BlockState blockstate, ServerWorld world, BlockPos pos, Random random) {
+			super.tick(blockstate, world, pos, random);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+
+			ProcessbasicwireProcedure.executeProcedure(Stream
+					.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
+							new AbstractMap.SimpleEntry<>("z", z))
+					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+			world.getPendingBlockTicks().scheduleTick(pos, this, 1);
+		}
+
+		@Override
+		public void onBlockPlacedBy(World world, BlockPos pos, BlockState blockstate, LivingEntity entity, ItemStack itemstack) {
+			super.onBlockPlacedBy(world, pos, blockstate, entity, itemstack);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+
+			ProcessbasicwireProcedure.executeProcedure(Stream
+					.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
+							new AbstractMap.SimpleEntry<>("z", z))
+					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+		}
+
+		@Override
+		public INamedContainerProvider getContainer(BlockState state, World worldIn, BlockPos pos) {
+			TileEntity tileEntity = worldIn.getTileEntity(pos);
+			return tileEntity instanceof INamedContainerProvider ? (INamedContainerProvider) tileEntity : null;
+		}
+
+		@Override
+		public boolean hasTileEntity(BlockState state) {
+			return true;
+		}
+
+		@Override
+		public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+			return new CustomTileEntity();
+		}
+
+		@Override
+		public boolean eventReceived(BlockState state, World world, BlockPos pos, int eventID, int eventParam) {
+			super.eventReceived(state, world, pos, eventID, eventParam);
+			TileEntity tileentity = world.getTileEntity(pos);
+			return tileentity == null ? false : tileentity.receiveClientEvent(eventID, eventParam);
+		}
+
+		@Override
+		public void onReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean isMoving) {
+			if (state.getBlock() != newState.getBlock()) {
+				TileEntity tileentity = world.getTileEntity(pos);
+				if (tileentity instanceof CustomTileEntity) {
+					InventoryHelper.dropInventoryItems(world, pos, (CustomTileEntity) tileentity);
+					world.updateComparatorOutputLevel(pos, this);
+				}
+
+				super.onReplaced(state, world, pos, newState, isMoving);
+			}
+		}
+
+		@Override
+		public boolean hasComparatorInputOverride(BlockState state) {
+			return true;
+		}
+
+		@Override
+		public int getComparatorInputOverride(BlockState blockState, World world, BlockPos pos) {
+			TileEntity tileentity = world.getTileEntity(pos);
+			if (tileentity instanceof CustomTileEntity)
+				return Container.calcRedstoneFromInventory((CustomTileEntity) tileentity);
+			else
+				return 0;
+		}
+	}
+
+	public static class CustomTileEntity extends LockableLootTileEntity implements ISidedInventory {
+		private NonNullList<ItemStack> stacks = NonNullList.<ItemStack>withSize(9, ItemStack.EMPTY);
+
+		protected CustomTileEntity() {
+			super(tileEntityType);
+		}
+
+		@Override
+		public void read(BlockState blockState, CompoundNBT compound) {
+			super.read(blockState, compound);
+			if (!this.checkLootAndRead(compound)) {
+				this.stacks = NonNullList.withSize(this.getSizeInventory(), ItemStack.EMPTY);
+			}
+			ItemStackHelper.loadAllItems(compound, this.stacks);
+			if (compound.get("energyStorage") != null)
+				CapabilityEnergy.ENERGY.readNBT(energyStorage, null, compound.get("energyStorage"));
+		}
+
+		@Override
+		public CompoundNBT write(CompoundNBT compound) {
+			super.write(compound);
+			if (!this.checkLootAndWrite(compound)) {
+				ItemStackHelper.saveAllItems(compound, this.stacks);
+			}
+			compound.put("energyStorage", CapabilityEnergy.ENERGY.writeNBT(energyStorage, null));
+			return compound;
+		}
+
+		@Override
+		public SUpdateTileEntityPacket getUpdatePacket() {
+			return new SUpdateTileEntityPacket(this.pos, 0, this.getUpdateTag());
+		}
+
+		@Override
+		public CompoundNBT getUpdateTag() {
+			return this.write(new CompoundNBT());
+		}
+
+		@Override
+		public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+			this.read(this.getBlockState(), pkt.getNbtCompound());
+		}
+
+		@Override
+		public int getSizeInventory() {
+			return stacks.size();
+		}
+
+		@Override
+		public boolean isEmpty() {
+			for (ItemStack itemstack : this.stacks)
+				if (!itemstack.isEmpty())
+					return false;
+			return true;
+		}
+
+		@Override
+		public ITextComponent getDefaultName() {
+			return new StringTextComponent("basicwireflate");
+		}
+
+		@Override
+		public int getInventoryStackLimit() {
+			return 64;
+		}
+
+		@Override
+		public Container createMenu(int id, PlayerInventory player) {
+			return ChestContainer.createGeneric9X3(id, player, this);
+		}
+
+		@Override
+		public ITextComponent getDisplayName() {
+			return new StringTextComponent("Basicwire 5");
+		}
+
+		@Override
+		protected NonNullList<ItemStack> getItems() {
+			return this.stacks;
+		}
+
+		@Override
+		protected void setItems(NonNullList<ItemStack> stacks) {
+			this.stacks = stacks;
+		}
+
+		@Override
+		public boolean isItemValidForSlot(int index, ItemStack stack) {
+			return true;
+		}
+
+		@Override
+		public int[] getSlotsForFace(Direction side) {
+			return IntStream.range(0, this.getSizeInventory()).toArray();
+		}
+
+		@Override
+		public boolean canInsertItem(int index, ItemStack stack, @Nullable Direction direction) {
+			return this.isItemValidForSlot(index, stack);
+		}
+
+		@Override
+		public boolean canExtractItem(int index, ItemStack stack, Direction direction) {
+			return true;
+		}
+
+		private final LazyOptional<? extends IItemHandler>[] handlers = SidedInvWrapper.create(this, Direction.values());
+		private final EnergyStorage energyStorage = new EnergyStorage(200, 200, 200, 0) {
+			@Override
+			public int receiveEnergy(int maxReceive, boolean simulate) {
+				int retval = super.receiveEnergy(maxReceive, simulate);
+				if (!simulate) {
+					markDirty();
+					world.notifyBlockUpdate(pos, world.getBlockState(pos), world.getBlockState(pos), 2);
+				}
+				return retval;
+			}
+
+			@Override
+			public int extractEnergy(int maxExtract, boolean simulate) {
+				int retval = super.extractEnergy(maxExtract, simulate);
+				if (!simulate) {
+					markDirty();
+					world.notifyBlockUpdate(pos, world.getBlockState(pos), world.getBlockState(pos), 2);
+				}
+				return retval;
+			}
+		};
+
+		@Override
+		public <T> LazyOptional<T> getCapability(Capability<T> capability, @Nullable Direction facing) {
+			if (!this.removed && facing != null && capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+				return handlers[facing.ordinal()].cast();
+			if (!this.removed && capability == CapabilityEnergy.ENERGY)
+				return LazyOptional.of(() -> energyStorage).cast();
+			return super.getCapability(capability, facing);
+		}
+
+		@Override
+		public void remove() {
+			super.remove();
+			for (LazyOptional<? extends IItemHandler> handler : handlers)
+				handler.invalidate();
+		}
+	}
+}
